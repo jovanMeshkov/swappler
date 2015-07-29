cc815be82fc7ddd68ee140229552426fc7d3a9e5
Jovan Meshkov <vanemeskov@hotmail.com>
2015-07-29 20:53:58 +0200

    Initial setup

---
+++ Swappler/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Swappler
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Public/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Public/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Public/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Public/Scripts/bootstrap.js",
+                      "~/Public/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                      "~/Public/Css/bootstrap.css",
+                      "~/Public/Css/site.css"));
+        }
+    }
+}
