51098280b2b52dfc0546299c6c240eaf08f273ac
Dane <mitrevdane@gmail.com>
2015-07-29 22:06:28 +0200

    Added models.

---
+++ Swappler/Models/SwapItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swappler.Models
+{
+    public class SwapItem
+    {
+        private String name { get; set; }
+        private String description { get; set; }
+        private DateTime date { get; set; }
+        private Boolean flag_swapped { get; set; }
+        private User userId { get; set; }
+        private Address address { get; set; }
+        private String photoURL { get; set; }
+    }
+}
---
+++ Swappler/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swappler.Models
+{
+    public class User
+    {
+        private String Name { get; set; }
+        private String lastName { get; set; }
+        private String email { get; set; }
+        private String password { get; set; }
+        private String username { get; set; }
+        private String phone { get; set; }
+        private String photoURL { get; set; }
+        private Address address { get; set; }
+    }
+}
